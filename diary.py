1de29bef55197ed55cd287afa1c1fa294fc227bb
piperfw <Piperfw@gmail.com>
2018-07-14 09:48:34 +0100

    Removed todo comment

--- diary.py
+++ diary.py
@@ -2,10 +2,6 @@ import json
 import datetime
 import re, sys, os, logging
 
-# Possible features
-# Additional fields: Duration, description and company
-# Configuration file; option for different date formats
-
 # level=logging.INFO for more information
 logging.basicConfig(level=logging.WARNING, format='%(asctime)s:%(name)s:%(levelname)s: %(message)s',
 	datefmt='%d/%m/%Y %H:%M:%S')
